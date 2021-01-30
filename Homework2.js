5ceb2c94d2cc429c5a6569dda5e636f299466ced
ibykova <bykova92@gmail.com>
2021-01-30 18:38:56 +0200

    finished all tasks

--- Homework2.js
+++ Homework2.js
@@ -1,8 +1,7 @@
 // Task 1
-
 let x = 1;
 let y = 2;
-let res1 = x + "" + y ;
+let res1 = x + "" + y;
 console.log(res1); // "12"
 console.log(typeof res1); // "string"
 
@@ -19,17 +18,14 @@ console.log(res4); // NaN
 console.log(typeof res4); // "number"
 
 // Task 2
-
 let numToCheck = prompt("Enter a number", "Number");
 if (0 === numToCheck % 2) console.log(numToCheck, "is Even.");
- else console.log(numToCheck, "is Odd.");
-
+else console.log(numToCheck, "is Odd.");
 
 if (numToCheck % 7 === 0) console.log(numToCheck, "is multiple of 7.");
 else console.log(numToCheck, "is NOT multiple of 7.");
 
 // Task 3
-
 let newArr = [];
 let newArrData = [5, "string", true, null];
 document.write(newArr.length);
@@ -41,27 +37,58 @@ newArrData.shift();
 console.log(newArrData);
 
 //Task 4
-
 let cities = ["Rome", "Lviv", "Warsaw"];
 console.log(cities.join("*"));
 
 // Task 5
-
-let isAdult = prompt("Enter your age", "Your age is");
-if (isAdult >= 18) {
+let isAdult = Number(prompt("Enter your age", "Your age is"));
+if (Number.isNaN(isAdult) || isAdult == "") {
+  alert("Only numbers required. Refresh the page and try again");
+  Number(prompt("Enter your age", "Your age is"));
+} else if (isAdult >= 18) {
   console.log("You are adult.");
 } else if (isAdult <= 10) {
   console.log("You are too young!");
-} else if (isAdult >= 10 && isAdult <= 18){
+} else {
   console.log("Your age is out of permitted range.");
 }
 
 // Task 6
-
-let a = prompt("Enter a number for side 'a':", "Your value is:");
-let b = prompt("Enter a number for side 'a':", "Your value is:");
-let c = prompt("Enter a number for side 'a':", "Your value is:");
-let sp = (a + b + c) / 2;
-let area = Math.sqrt(sp * ((sp - a) * (sp - b) * (sp - c)));
+let a, b, c;
+function askSides() {
+  a = Number(prompt("Enter a number for side (a):"));
+  b = Number(prompt("Enter a number for side (b):"));
+  c = Number(prompt("Enter a number for side (c):"));
+}
+askSides();
+let sp = (a + b + c) / 2; // semiperimeter
+let calc = sp * ((sp - a) * (sp - b) * (sp - c));
+if (calc < 0) {
+  alert("Couldn't create a trianle with these values.");
+  askSides();
+} else if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert("Incorrect data");
+  askSides();
+}
+let area = Math.sqrt(calc);
 console.log(area.toFixed(3));
+
+((a * b) / 2) === area ? console.log("This triangle is right-angled.") : console.log("This triangle is NOT right-angled.");
+
+//Task 7
+let currHours = new Date().getHours();
+let greeting = "Good night!";
+
+if (currHours > 17 && currHours <= 23) greeting = "Good evening!";
+else if (currHours > 5 && currHours <= 11) greeting = "Good morning!";
+else if (currHours > 11 && currHours <= 17) greeting = "Good afternoon!";
+
+console.log(greeting);
+
+greeting = currHours > 17 && currHours <= 23 ? "Good evening!"
+  : currHours > 5 && currHours <= 11 ? "Good morning!"
+    : currHours > 11 && currHours <= 17 ? "Good afternoon!"
+      : "Good night!";
+
+console.log(greeting);
 
