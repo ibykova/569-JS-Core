12944c3cc03d5e0130525bd6369e88d7dd9e7b5c
ibykova <bykova92@gmail.com>
2021-02-15 21:50:23 +0200

    Homework5 finalized

--- Homework5.js
+++ Homework5.js
@@ -75,32 +75,47 @@ console.log("Current course: " + std1.showCourse());
 class Worker {
   #experience = 1.2;
 
-  constructor (fullName, dayRate, workingDays) {
+  constructor(fullName, dayRate, workingDays) {
     this.fullName = fullName;
     this.dayRate = dayRate;
     this.workingDays = workingDays;
   }
   showSalary() {
-   let result = this.dayRate * this.workingDays;
-   console.log(result);
-   return result;
+    let result = this.dayRate * this.workingDays;
+    console.log(result);
+    return result;
   }
   showSalaryWithExperience() {
     let result = this.showSalary() * this.#experience;
     console.log(result);
     return result;
   }
+
+  showSalaryWorker() {
+    let result = this.dayRate * this.workingDays * this.#experience;
+    console.log(result);
+    return result
+  }
+
   get showExp() {
     return this.#experience;
   }
   set setExp(value) {
     this.#experience = value;
-    // Worker.#experience = 1.5;
     console.log(value);
+  }
+  sortSalaries(workersArray) {
+
+    let sortedSalary = workersArray.sort(function (a, b) {
+      return a.showSalaryWorker() - b.showSalaryWorker();
+    })
+    for (let i = 0; i < sortedSalary.length; i++) {
+      console.log(sortedSalary[i].fullName + ": " + sortedSalary[i].showSalaryWorker());
+    }
   }
 }
 
-let worker1 = new Worker ('Peter Peterson', 48, 22);
+let worker1 = new Worker('Peter Peterson', 48, 22);
 console.log(worker1.fullName)
 worker1.showSalary();
 console.log("New experience: " + worker1.showExp);
@@ -109,7 +124,7 @@ worker1.setExp = 1.5;
 console.log("New experience: " + worker1.showExp);
 worker1.showSalaryWithExperience();
 
-let worker2 = new Worker ('John Johnson', 20, 23);
+let worker2 = new Worker('John Johnson', 20, 23);
 console.log(worker2.fullName)
 worker2.showSalary();
 console.log("New experience: " + worker2.showExp);
@@ -118,7 +133,7 @@ worker2.setExp = 1.5;
 console.log("New experience: " + worker2.showExp);
 worker2.showSalaryWithExperience();
 
-let worker3 = new Worker ('Andy Anderson', 29, 23);
+let worker3 = new Worker('Andy Anderson', 29, 23);
 console.log(worker3.fullName)
 worker3.showSalary();
 console.log("New experience: " + worker3.showExp);
@@ -127,10 +142,73 @@ worker3.setExp = 1.5;
 console.log("New experience: " + worker3.showExp);
 worker3.showSalaryWithExperience();
 
+let workersArray = [worker1, worker2, worker3];
+worker3.sortSalaries(workersArray);
+
+
 // let workerSalary = [worker1, worker2, worker3];
-// let sortedSalary = [];
 // workerSalary.forEach(w => {
 //   console.log("Sorted salary " + w.showSalaryWithExperience())
 // })
-// workerSalary.sort(function(a, b){return a - b});
+// let sortedSalary = workerSalary.sort((a, b) => {
+//   return a.showSalaryWithExperience() - b.showSalaryWithExperience()
+// });
+// console.log(sortedSalary);
 
+
+
+// Task 5
+
+class GeometricFigure {
+  getArea() {
+    return 0;
+  }
+  toString() {
+    return Object.getPrototypeOf(this).constructor.name;
+  }
+}
+
+class Triangle extends GeometricFigure {
+  constructor(b, h) {
+    super();
+    this.base = b;
+    this.height = h;
+  }
+  getArea() {
+    return this.base * this.height /2;
+  }
+}
+
+class Square extends GeometricFigure {
+  constructor(a) {
+    super();
+    this.side = a;
+  }
+  getArea() {
+    return this.side **2;
+  }
+}
+
+class Circle extends GeometricFigure {
+  constructor(r) {
+    super();
+    this.radius = r;
+  }
+  getArea() {
+    return Math.PI * this.radius **2;
+  }
+}
+
+function handleFigures(figures) {
+  return figures.reduce(function(sum,figure) {
+    if (figure instanceof GeometricFigure) {
+      console.log(`GeometricFigure ${figure.toString()} - area: ${figure.getArea()}`);
+      return sum + figure.getArea();
+    }
+    throw Error ("Bad argument figure.");
+  }, 0);
+}
+
+let figures = [new Triangle(4, 5), new Square(7), new Circle(5)];
+
+console.log(handleFigures(figures));
